28b0d10864191fa840f40923b91e20bf4cfa0c67
Hemanshu Mahajan <hemanshumahajan21@gmail.com>
2026-03-14 23:27:53 +0530

    Add backend deployment domain

--- portfolio-backend/Program.cs
+++ portfolio-backend/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy => 
         policy.WithOrigins("http://localhost:5173",
-        "https://your-frontend-url.com") // ? replace with your real domain)
+        "https://hemanshusportfolio.onrender.com") 
         .AllowAnyHeader() 
         .AllowAnyMethod()));
 
