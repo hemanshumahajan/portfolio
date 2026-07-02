eddf4dfb7fe515db247e9c9fa4c7b0dc9340e104
Hemanshu Mahajan <hemanshumahajan21@gmail.com>
2026-07-02 17:34:59 +0530

    Updated CORS

--- portfolio-backend/Program.cs
+++ portfolio-backend/Program.cs
@@ -71,7 +71,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
         policy.WithOrigins("http://localhost:5173",
-        "https://hemanshumahajan.vercel.app")
+        "https://hemanshumahajan.vercel.app",
+        "https://hemanshumahajan.com",
+        "https://www.hemanshumahajan.com")
         .AllowAnyHeader()
         .AllowAnyMethod()));
 
